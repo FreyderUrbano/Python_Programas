ba28e57a133e4549415fa153afac9018956fb623
FreyderUrbano <freurbano@gmail.com>
2021-08-26 11:44:03 -0500

    update 26/08/2021

---
+++ ControladoresAritmeticos.py
@@ -0,0 +1,58 @@
+print ("ESTA ES LA SUMA")
+numeroA = 5
+numeroB = 20
+resultado = numeroA+numeroB
+print("EL RESULTADO DE LA SUMA ES: " + str(resultado))
+
+print ("ESTA ES LA RESTA")
+numeroA = 5
+numeroB = 20
+resultado = numeroA + numeroB
+print("EL RESULTADO DE LA SUMA ES: " + str(resultado))
+
+
+print ("ESTA ES LA RESTA")
+numeroA = 5
+numeroB = 20
+resultado = numeroA - numeroB
+print("EL RESULTADO DE LA RESTA ES: " + str(resultado))
+
+print ("ESTA ES LA MULTIPLICACION")
+numeroA = 5
+numeroB = 20
+resultado = numeroA * numeroB
+print("EL RESULTADO DE LA MULTIPLICACION ES: " + str(resultado))
+
+print ("ESTA ES LA EXPONENCIAL")
+numeroA = 2
+exponente = 10 
+resultado = numeroA ** exponente
+print("EL RESULTADO DEL EXPONENTE ES : " + str(resultado))
+
+
+print ("ESTA ES LA DIVICION")
+numeroA = 20
+numeroB = 3
+resultado = numeroA / numeroB
+print("EL RESULTADO DE LA DIVICION ES: " + str(resultado))
+
+print ("ESTE ES EL MODULO")
+numeroA = 30
+numeroB = 8
+resultado = numeroA % numeroB
+print("EL RESULTADO DEL MODULO ES: " + str(resultado))
+
+
+print ("ESTA ES DIVICION ENTERA")
+numeroA = 20
+numeroB = 3
+resultado = numeroA // numeroB
+print("EL RESULTADO DE LA DIVICION ENTERA ES :: " + str(resultado))
+
+
+
+
+
+
+
+
